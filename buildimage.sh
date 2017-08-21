d6424f35ab9a5b081ef83c73e898e04ad04e582c
thendawg <npound7110@gmail.com>
2017-08-20 20:44:38 -0500

    Add files via upload

--- buildimage.sh
+++ buildimage.sh
@@ -4,17 +4,18 @@
 apt-get -y install git
 
 # Setup nVidia Drivers
-add-apt-repository -y ppa:graphics-drivers
-apt-get -y update
-apt-get -y install nvidia-384
+mkdir /nvidia/
+cd /nvidia/
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/384.59/NVIDIA-Linux-x86_64-384.59.run
+chmod +x NVIDIA-Linux-x86_64-384.59.run
+sh ./384.59/NVIDIA-Linux-x86_64-384.59.run
 
 # Setup CUDA 8
 mkdir /cuda/
 cd /cuda/
-wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
-dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
-apt-get -y update
-apt-get -y install cuda
+wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
+chmod +x cuda_8.0.61_375.26_linux-run
+sh ./cuda_8.0.61_375.26_linux-run
 
 # Install Apache/PHP
 apt-get -y install apache2
@@ -52,6 +53,7 @@ chmod -R 755 /claymore/
 
 # Cleanup
 rm -rf /cuda/
+rm -rf /nvidia/
 
 # Setup Service
 /bin/cp -f /nvezos/scripts/system/runonce.service /etc/systemd/system/
