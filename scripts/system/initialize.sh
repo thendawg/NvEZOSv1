9d735476c146e1b0848e4991144db1baca894382
thendawg <npound7110@gmail.com>
2017-08-22 18:50:47 -0500

    Add files via upload

--- scripts/system/initialize.sh
+++ scripts/system/initialize.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 3
 sudo rm -rf /etc/X11/xorg.conf
 sudo nvidia-xconfig -a --cool-bits=31 --allow-empty-initial-configuration
 sudo sed -i '/Option         "AllowEmptyInitialConfiguration" "True"/a    Option         "ConnectedMonitor" "DFP-0"' /etc/X11/xorg.conf
@@ -9,5 +10,5 @@ sleep 2
 sudo systemctl enable minerdev.service
 sudo systemctl daemon-reload
 sudo mv /nvezos/scripts/system/initialize.sh /nvezos/scripts/system/initold.sh
-sleep 10
+sleep 7
 sudo shutdown -r now
